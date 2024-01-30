61528a122d5e379b1a5115a8534bb6b253d019ed
alexxcalle <146011365+alexxcalle@users.noreply.github.com>
2024-01-29 19:54:48 -0500

    termiando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MenuComponent, HeaderComponent,LoginComponent],
+  imports: [CommonModule, RouterOutlet, MenuComponent, HeaderComponent,LoginComponent, BodyComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
